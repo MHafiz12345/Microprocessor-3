e9fc990c2e150e979eb755d1651728cbef81bb98
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2025-04-19 02:25:34 +0700

    Update lcd.cpp

--- Lab 3 blank/src/lcd.cpp
+++ Lab 3 blank/src/lcd.cpp
@@ -1,10 +1,3 @@
-//----------------------------------------------------------------------//
-// Author:   Muhammad Hafiz      
-// Net ID:   muhammadhafiz      
-// Date:     03 March 2025     
-// Assignment: Lab 3
-//----------------------------------------------------------------------//
-
 #include "lcd.h"
 #include "timer.h"
 #include <util/delay.h>
